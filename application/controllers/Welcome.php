88653ad757976e77f70502737929386f1ec60682
Chris Klassen <ca.klassen95@gmail.com>
2015-03-25 16:51:42 -0700

    Finished lab 7 Signed-off-by:Chris Klassen <ca.klassen95@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,19 +21,31 @@ class Welcome extends Application {
 
     function index()
     {
+        $this->load->model('order');
+        
 	// Build a list of orders
         $dir = directory_map('./data/');
+        $files = array();
         
         // Loop through each file
         foreach ($dir as $file)
         {
             // If the file is an XML file
-            if (strpos($file, '.xml') !== false)
-            {
-                echo $file;
+            if (strpos($file, 'order') !== false && strpos($file, '.xml') !== false)
+            {        
+                $order = new Order($file);
+                
+                // Add the filename to an array
+                $files[] = array(
+                    'filename' => substr($file, 0, strlen($file) - 4),
+                    'customer' => $order->customer
+                );
             }
         }
-	
+
+        // Put the files into the view
+	$this->data['orders'] = $files;
+        
 	// Present the list to choose from
 	$this->data['pagebody'] = 'homepage';
 	$this->render();
@@ -45,8 +57,17 @@ class Welcome extends Application {
 
     function order($filename)
     {
+        $this->load->model('order');
+        $order = new Order($filename . '.xml');
+        
 	// Build a receipt for the chosen order
-	
+	$this->data['filename'] = $filename;
+        $this->data['customer'] = $order->customer;
+        $this->data['type'] = $order->type;
+        $this->data['burgers'] = $order->burgers;
+        $this->data['total'] = $order->total;
+        $this->data['special'] = $order->orderInstructions;
+        
 	// Present the list to choose from
 	$this->data['pagebody'] = 'justone';
 	$this->render();
